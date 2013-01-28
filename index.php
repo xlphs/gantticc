bea750f33ff203f71808e35fcbce1e2456ab298a
Marika Wei <dev@miphol.com>
2013-01-27 21:41:47 -0500

    New task icon

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 		<title>Gantticc</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge;chrome=1" >
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1.0">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 	</head>
 	<body>
 		Welcome to Gantticc. <a href="test.html">Take a test drive.</a>
